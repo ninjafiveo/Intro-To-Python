1044b4398864785019f7ea4b0b2582a92011b28f
ninjafiveo <ninjafiveo@gmail.com>
2021-05-09 23:50:50 -0400

    Updated Function

---
+++ 5Functions.py
@@ -0,0 +1,11 @@
+greeting = "Welcome to the game, "
+question1 = "Welcome young warrior, what is your name? "
+response1 = input(question1) #input
+
+#Function
+def intro():
+    print(greeting + response1 + ". It is glorius to have you joining us on this journey.")
+
+#Call the Function
+intro()
+
