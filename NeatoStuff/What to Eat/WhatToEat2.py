70c4776a6ef61eda5f7eb1019a4f1cc93c6c7c44
ninjafiveo <42717162+ninjafiveo@users.noreply.github.com>
2021-05-12 13:56:34 -0400

    refactored

--- NeatoStuff/What to Eat/WhatToEat2.py
+++ NeatoStuff/What to Eat/WhatToEat2.py
@@ -1,53 +1,84 @@
 import random
-# cuisineSelection = {
-#     "Chinese":{"House of China", "Imperial Garden", "Golden Hunan"},
-#     "Japanese":{"Izumi", "Mizu", "Tokyo Housu"},
-#     "Mexican":{"Sr. Jalepeno", "Los Gallos", "El Vallarta"},
-#     "Indian":{"Cafe Indian", "Bombay Curry and Grill", "Nick & Mintu da dhaba"},
-#     "Pizza":{"Nicky's", "Bruno Brothers", "West Gate"},
-#     "Fast Food":{"Chic Fil A", "Panda Express", "Popeyes Chicken"},
-#     #"Italian":{"Salvatores", "Scarsellas", "Nicolinni's"},
-# }
-# print(cuisineSelection["Chinese"][0][1])
-
-# val = random.choice(list(cuisineSelection.items()))
-# print(val)
-
-chinese_food = ["House of China", "Imperial Garden", "Golden Hunan", "Main Moon", "Chinatown Inn"]
-japanese_food = ["Izumi", "Mizu", "Tokyo House", "Sakura", "Asuka"]
-mexican_food = ["Sr. Jalepeno", "Los Gallos", "El Vallarta"]
-indian_food =["Cafe Indian", "Bombay Curry and Grill", "Nick & Mintu da dhaba"]
-pizza_food =["Nicky's", "Bruno Brothers", "West Gate"]
-fast_food = ["Chic Fil A", "Panda Express", "Popeyes Chicken"]
-italian_food = ["Salvatores", "Scarsellas", "Nicolinni's"]
+cuisine_selection = {
+    "Chinese":("House of China", "Imperial Garden", "Golden Hunan"),
+    "Japanese":("Izumi", "Mizu", "Tokyo House"),
+    "Mexican":("Sr. Jalepeno", "Los Gallos", "El Vallarta"),
+    "Indian":("Cafe Indian", "Bombay Curry and Grill", "Nick & Mintu da dhaba"),
+    "Pizza":("Nicky's", "Bruno Brothers", "West Gate"),
+    "Fast Food":("Chic Fil A", "Panda Express", "Popeyes Chicken"),
+    "Italian":("Salvatores", "Scarsellas", "Nicolinni's"),
+}
 
 cuisine_choice_responses = ["Excellent Choice. ", "Mmmmm... That's a delicious idea. ", "Oh yes. I'm getting fat tonight. ", "One of my favs. ", "My belly is happy. "]
-
 random_response = random.choice(cuisine_choice_responses)
 
-# print(random.choice(chinese_food))
 def welcome():
-    print("Hello there, looks like you are having trouble deciding what to eat.")
-    cuisine_choice = input("Please select what type of cuisine you prefer: \n1 = Chinese\n2 = Japanese\n3 = Mexican\n4 = Indian \n5 = Pizza \n6 = Fast Food\n7 = Italian\n8 = I Don't Know!\nPlease enter a number: ")
-    if cuisine_choice == "1":
-        print(random_response+"You should eat at: "+random.choice(chinese_food))
-    elif cuisine_choice == "2":
-        print(random_response+"You should eat at: "+random.choice(japanese_food))
-    elif cuisine_choice == "3":
-        print(random_response+"You should eat at: "+random.choice(mexican_food))
-    elif cuisine_choice == "4":
-        print(random_response+"You should eat at: "+random.choice(indian_food))
-    elif cuisine_choice == "5":
-        print(random_response+"You should eat at: "+random.choice(pizza_food))
-    elif cuisine_choice == "6":
-        print(random_response+"You should eat at: "+random.choice(fast_food))
-    elif cuisine_choice == "7":
-        print(random_response+" You should eat at: "+random.choice(italian_food))
-    elif cuisine_choice == "8":
-        
-        print(random_response+" You should eat at: "+random.choice(italian_food))
+    print("Hello there, looks like you are having trouble deciding what to eat. I'll help.")
+    cuisine_choice = int(input("Please select what type of cuisine you prefer: \n1 = Chinese\n2 = Japanese\n3 = Mexican\n4 = Indian \n5 = Pizza \n6 = Fast Food\n7 = Italian\n8 = I Don't Know!\nPlease enter a number: "))
+    cuisine_choice -= 1
+    if cuisine_choice >= 1 and cuisine_choice <= 8:
+        print(random_response + "Have fun eating: "+list(cuisine_selection)[cuisine_choice])
     else:
         print("\033[1;32;47m You did not select a number. Please try again. ")
-        #https://ozzmaker.com/add-colour-to-text-in-python/
         welcome()
 welcome()
+
+
+
+
+#################
+#OLD CODE
+
+#Previous def welcome Contents
+    # if cuisine_choice == "1":
+    #     print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    # elif cuisine_choice == "2":
+    #     print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    # elif cuisine_choice == "3":
+    #     print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    # elif cuisine_choice == "4":
+    #     print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    # elif cuisine_choice == "5":
+    #     print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    # elif cuisine_choice == "6":
+    #     print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    # elif cuisine_choice == "7":
+    #     print(random_response+" You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    # elif cuisine_choice == "8":
+    #     my_random_choice = random.randint(1,8)
+    #     if my_random_choice == 1:
+    #         print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    #     elif my_random_choice == 2:
+    #         print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    #     elif my_random_choice == 3:
+    #         print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    #     elif my_random_choice == 4:
+    #         print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    #     elif my_random_choice == 5:
+    #         print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    #     elif my_random_choice == 6:
+    #         print(random_response+"You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    #     else:
+    #         print(random_response+" You should eat at: "+random.choice(cuisine_selection["Chinese"]))
+    # else:
+    #     print("\033[1;32;47m You did not select a number. Please try again. ")
+    #     #https://ozzmaker.com/add-colour-to-text-in-python/
+    #     welcome()
+
+
+
+    ####
+
+
+    # print(test1)
+# print(test2)
+# print(test3)
+
+# print(list(cuisine_selection)[0])
+# print(test3)
+# print(random.choice(cuisine_selection["Chinese"]))#choose a random value of Chinese
+# print(random.choice(cuisine_selection["Japanese"]))#choose a random value of Japanese
+# # print(random.choice(cuisine_selection.keys()))
+# test1 = random.choice(list(cuisine_selection)) # Chooses a random choice of the Keys in cuisine selection
+# test2 = random.choice(list(cuisine_selection)[0]) # Chooses a random letter choice of the word index at 0... aka Chinese.
+# test3 = list(cuisine_selection)[1] # Makes the keys a list and selects the list item at index 1... aka Japanese.
